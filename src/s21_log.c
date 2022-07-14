79abb15ad1673b078518c589794d1b156d633496
Worker Corrino <workerco@ga-j2.msk.21-school.ru>
2022-07-14 16:06:21 +0300

    :hammer: update(log-pow): log WIP via Galley method?

--- src/s21_log.c
+++ src/s21_log.c
@@ -1,6 +1,24 @@
 #include "s21_math.h"
 
-long double s21_cos(double x) {
-    return x;
+long double s21_log(double x) {
+    // double alpha = (x-1)/(x+1), ans = alpha;
+    // double save = ans * alpha * alpha;
+
+    // for (register int i = 2 ; i <= 10000 ; i++) {
+    //     ans += (1.0/(2*i-1)) * save;
+    //     save = save * alpha * alpha;
+    // }
+
+//     return (2.0 * ans);
+    int ex_pow = 0;
+    double result = 0, compare = result;
+
+    for (; x >= S21_EXP; x /= S21_EXP, ex_pow++) continue;
+
+    for (int i = 0; i < 100; i++) {
+        compare = result;
+        result = compare + 2 * (x - s21_exp(compare)) / (x + s21_exp(compare));
+    }
+    return (long double) result + ex_pow;
 }
 
