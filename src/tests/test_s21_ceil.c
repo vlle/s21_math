23a888290bb3db121234cfd111ee26163bcc4705
Worker Corrino <workerco@ga-j2.msk.21-school.ru>
2022-07-14 18:41:36 +0300

    :shirt: refactor(tests): refactor WIP

---
+++ src/tests/test_s21_ceil.c
@@ -0,0 +1,54 @@
+#include <check.h>
+#include "../s21_math.h"
+
+START_TEST(s21_ceilClassic) {
+    double x = 83.9283;
+    long double my_ans = s21_ceil(x);
+    long double true_ans = ceil(x);
+    ck_assert_ldouble_eq_tol(my_ans, true_ans, 1e-7);
+} END_TEST
+
+START_TEST(s21_ceilBig) {
+    double x = 1231783.9283;
+    long double my_ans = s21_ceil(x);
+    long double true_ans = ceil(x);
+    ck_assert_ldouble_eq_tol(my_ans, true_ans, 1e-7);
+} END_TEST
+
+START_TEST(s21_ceilZero) {
+    double x = 0;
+    long double my_ans = s21_ceil(x);
+    long double true_ans = ceil(x);
+    ck_assert_ldouble_eq_tol(my_ans, true_ans, 1e-7);
+} END_TEST
+
+START_TEST(s21_ceil_negative) {
+    double x = -783.9283;
+    long double my_ans = s21_ceil(x);
+    long double true_ans = ceil(x);
+    ck_assert_ldouble_eq_tol(my_ans, true_ans, 1e-7);
+} END_TEST
+
+START_TEST(s21_ceil_positive) {
+    double x = 3671.43561;
+    long double my_ans = s21_ceil(x);
+    long double true_ans = ceil(x);
+    ck_assert_ldouble_eq_tol(my_ans, true_ans, 1e-7);
+} END_TEST
+
+Suite* ceil_suite(void) {
+    Suite* s;
+    TCase* tc_core;
+
+    s = suite_create("ceil");
+    tc_core = tcase_create("Core");
+
+    tcase_add_test(tc_core, s21_ceilClassic);
+    tcase_add_test(tc_core, s21_ceilBig);
+    tcase_add_test(tc_core, s21_ceilZero);
+    tcase_add_test(tc_core, s21_ceil_negative);
+    tcase_add_test(tc_core, s21_ceil_positive);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
---
+++ src/tests/test_s21_sqrt.c
@@ -0,0 +1,47 @@
+#include "../s21_math.h"
+#include <check.h>
+
+START_TEST(s21_sqrtZero) {
+    double y = 0;
+    long double my_ans = s21_sqrt(y);
+    long double true_ans = sqrt(y);
+    ck_assert_ldouble_eq_tol(my_ans, true_ans, 1e-7);
+} END_TEST
+
+START_TEST(s21_sqrtBig) {
+    double y = 1131112.1232131231421;
+    long double my_ans = s21_sqrt(y);
+    long double true_ans = sqrt(y);
+    ck_assert_ldouble_eq_tol(my_ans, true_ans, 1e-7);
+} END_TEST
+
+START_TEST(s21_sqrtClassic) {
+    double y = 1.1232131231421;
+    long double my_ans = s21_sqrt(y);
+    long double true_ans = sqrt(y);
+    ck_assert_ldouble_eq_tol(my_ans, true_ans, 1e-7);
+} END_TEST
+
+START_TEST(s21_sqrtLessZero) {
+    double y = -123112.412;
+    long double my_ans = s21_sqrt(y);
+    long double true_ans = sqrt(y);
+    ck_assert_ldouble_eq_tol(my_ans, true_ans, 1e-7);
+} END_TEST
+
+
+Suite* sqrt_suite(void) {
+    Suite* s;
+    TCase* tc_core;
+
+    s = suite_create("sqrt");
+    tc_core = tcase_create("Core");
+
+    tcase_add_test(tc_core, s21_sqrtZero);
+    tcase_add_test(tc_core, s21_sqrtBig);
+    tcase_add_test(tc_core, s21_sqrtClassic);
+    tcase_add_test(tc_core, s21_sqrtLessZero);
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
