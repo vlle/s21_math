ec7a2dd5344f1d9f740469c2fd68b580b606cb26
Nazeebo Delma <nazeebod@oa-i5.msk.21-school.ru>
2022-07-14 16:35:53 +0300

    :shirt: refactor(s21_floor): fix cpplint err

--- src/s21_floor.c
+++ src/s21_floor.c
@@ -2,11 +2,11 @@
 //  returns the nearest integer not greater than the given value
 
 long double s21_floor(double x) {
-	if (is_inf(x)) {
-		return S21_INF;
-	}
-	if (is_nan(x)) {
-		return S21_NAN;
-	}
-	return (int) x;
+    if (is_inf(x)) {
+        return S21_INF;
+    }
+    if (is_nan(x)) {
+        return S21_NAN;
+    }
+    return (int) x;
 }
