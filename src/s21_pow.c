6ed56321765360e927efa7cfa54a3f597e348ec8
Nazeebo Delma <nazeebod@oa-i5.msk.21-school.ru>
2022-07-14 15:02:55 +0300

    :bug: bug(s21_pow): zero case

--- src/s21_pow.c
+++ src/s21_pow.c
@@ -1,5 +1,6 @@
 #include "s21_math.h"
 #include <math.h>
+#include <stdio.h>
 
 long double s21_pow(double base, double exps) {
     long double n = base;
@@ -7,15 +8,18 @@ long double s21_pow(double base, double exps) {
     if (exps == 0) {
         res = 1;
     } else {
-        if (n < 0) {
-            res = exp(exps*log(-n));
-            if (s21_fmod(exps, 2) != 0) {
-                res = res * -1;
-            }
+        if (base == 0) {
+            res = 0;
         } else {
-            res = exp(exps*log(n));
+            if (n < 0) {
+                res = s21_exp(exps*log(-n));
+                if (s21_fmod(exps, 2) != 0) {
+                    res = res * -1;
+                }
+            } else {
+                res = s21_exp(exps*log(n));
+            }
         }
     }
     return res;
 }
-
