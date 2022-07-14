050ed13450f0954cfe2175520bafb1cc0c3e4fb1
Worker Corrino <workerco@ga-j2.msk.21-school.ru>
2022-07-14 17:52:52 +0300

    :white_check_mark: refactor(test): refactor tests WIP

--- src/math_tests.c
+++ src/math_tests.c
@@ -2,6 +2,7 @@
 #include <stdio.h>
 #include <math.h>
 #include <stdlib.h>
+#include "tests/s21_math_test.h"
 #include "s21_math.h"
 
 START_TEST(s21_ceilClassic) {
@@ -39,42 +40,42 @@ START_TEST(s21_ceil_positive) {
     ck_assert_ldouble_eq_tol(my_ans, true_ans, 1e-7);
 } END_TEST
 
-START_TEST(s21_acos_too_negative) {
-    double x = -12.781;
-    long double my_ans = s21_acos(x);
-    long double true_ans = acos(x);
-    ck_assert_ldouble_nan(my_ans);
-    ck_assert_ldouble_nan(true_ans);
-} END_TEST
-
-START_TEST(s21_acos_negative) {
-    double x = -0.67218;
-    long double my_ans = s21_acos(x);
-    long double true_ans = acos(x);
-    ck_assert_ldouble_eq_tol(my_ans, true_ans, 1e-7);
-} END_TEST
-
-START_TEST(s21_acos_zero) {
-    double x = 1.0;
-    long double my_ans = s21_acos(x);
-    long double true_ans = acos(x);
-    ck_assert_ldouble_eq_tol(my_ans, true_ans, 1e-7);
-} END_TEST
-
-START_TEST(s21_acos_positive) {
-    double x = 0.4125638719;
-    long double my_ans = s21_acos(x);
-    long double true_ans = acos(x);
-    ck_assert_ldouble_eq_tol(my_ans, true_ans, 1e-7);
-} END_TEST
-
-START_TEST(s21_acos_too_positive) {
-    double x = 5.64278;
-    long double my_ans = s21_acos(x);
-    long double true_ans = acos(x);
-    ck_assert_ldouble_nan(my_ans);
-    ck_assert_ldouble_nan(true_ans);
-} END_TEST
+// START_TEST(s21_acos_too_negative) {
+//     double x = -12.781;
+//     long double my_ans = s21_acos(x);
+//     long double true_ans = acos(x);
+//     ck_assert_ldouble_nan(my_ans);
+//     ck_assert_ldouble_nan(true_ans);
+// } END_TEST
+//
+// START_TEST(s21_acos_negative) {
+//     double x = -0.67218;
+//     long double my_ans = s21_acos(x);
+//     long double true_ans = acos(x);
+//     ck_assert_ldouble_eq_tol(my_ans, true_ans, 1e-7);
+// } END_TEST
+//
+// START_TEST(s21_acos_zero) {
+//     double x = 1.0;
+//     long double my_ans = s21_acos(x);
+//     long double true_ans = acos(x);
+//     ck_assert_ldouble_eq_tol(my_ans, true_ans, 1e-7);
+// } END_TEST
+// 
+// START_TEST(s21_acos_positive) {
+//     double x = 0.4125638719;
+//     long double my_ans = s21_acos(x);
+//     long double true_ans = acos(x);
+//     ck_assert_ldouble_eq_tol(my_ans, true_ans, 1e-7);
+// } END_TEST
+// 
+// START_TEST(s21_acos_too_positive) {
+//     double x = 5.64278;
+//     long double my_ans = s21_acos(x);
+//     long double true_ans = acos(x);
+//     ck_assert_ldouble_nan(my_ans);
+//     ck_assert_ldouble_nan(true_ans);
+// } END_TEST
 
 START_TEST(s21_fmod_zero_division) {
     double x = 42647.78;
@@ -687,24 +688,24 @@ Suite* fmod_suite(void) {
     return s;
 }
 
-Suite* acos_suite(void) {
-    Suite* s;
-    TCase* tc_core;
-
-    s = suite_create("acos");
-    tc_core = tcase_create("Core");
-
-    tcase_add_test(tc_core, s21_acos_too_negative);
-    tcase_add_test(tc_core, s21_acos_negative);
-    tcase_add_test(tc_core, s21_acos_zero);
-    tcase_add_test(tc_core, s21_acos_positive);
-    tcase_add_test(tc_core, s21_acos_too_positive);
-
-    suite_add_tcase(s, tc_core);
-
-    return s;
-}
-
+// Suite* acos_suite(void) {
+//     Suite* s;
+//     TCase* tc_core;
+// 
+//     s = suite_create("acos");
+//     tc_core = tcase_create("Core");
+// 
+//     tcase_add_test(tc_core, s21_acos_too_negative);
+//     tcase_add_test(tc_core, s21_acos_negative);
+//     tcase_add_test(tc_core, s21_acos_zero);
+//     tcase_add_test(tc_core, s21_acos_positive);
+//     tcase_add_test(tc_core, s21_acos_too_positive);
+// 
+//     suite_add_tcase(s, tc_core);
+// 
+//     return s;
+// }
+// 
 Suite* sin_suite(void) {
     Suite* s;
     TCase* tc_core;
---
+++ src/tests/s21_math_test.h
@@ -0,0 +1,8 @@
+#ifndef SRC_TESTS_S21_MATH_TESTS_H
+#define SRC_TESTS_S21_MATH_TESTS_H
+#include "../s21_math.h"
+#include <check.h>
+
+Suite* acos_suite(void);
+
+#endif  // SRC_TESTS_S21_MATH_TESTS_H
