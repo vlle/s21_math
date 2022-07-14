8f40f7c8163aac6f3d067be2eaf3541cfbf2d065
Nazeebo Delma <nazeebod@il-i3.msk.21-school.ru>
2022-07-14 16:57:12 +0300

    :white_check_mark: test(log): add log suite

--- src/math_tests.c
+++ src/math_tests.c
@@ -404,7 +404,7 @@ START_TEST(s21_powLessZero) {
     double base = 3;
     long double a2 = pow(x, base);
     long double b2 = s21_pow(x, base);
-    ck_assert_ldouble_eq_tol(a2, b2, 1e-7);
+    ck_assert_ldouble_eq_tol(a2, b2, 1e-07);
 } END_TEST
 
 START_TEST(s21_powDouble1) {
@@ -493,6 +493,41 @@ START_TEST(s21_expBelowzero) {
     double x = -2;
     long double a2 = exp(x);
     long double b2 = s21_exp(x);
+    ck_assert_ldouble_eq_tol(a2, b2, 1e-7);
+} END_TEST
+
+START_TEST(s21_logClassic) {
+    double x = 3;
+    long double a2 = log(x);
+    long double b2 = s21_log(x);
+    ck_assert_ldouble_eq_tol(a2, b2, 1e-7);
+} END_TEST
+
+START_TEST(s21_logBig) {
+    double x = 3132;
+    long double a2 = log(x);
+    long double b2 = s21_log(x);
+    ck_assert_ldouble_eq_tol(a2, b2, 1e-7);
+} END_TEST
+
+START_TEST(s21_logLow) {
+    double x = -123;
+    long double a2 = log(x);
+    long double b2 = s21_log(x);
+    ck_assert_ldouble_eq_tol(a2, b2, 1e-7);
+} END_TEST
+
+START_TEST(s21_logZero) {
+    double x = 0;
+    long double a2 = log(x);
+    long double b2 = s21_log(x);
+    ck_assert_ldouble_eq_tol(a2, b2, 1e-7);
+} END_TEST
+
+START_TEST(s21_logBelowzero) {
+    double x = -2;
+    long double a2 = log(x);
+    long double b2 = s21_log(x);
     ck_assert_ldouble_eq_tol(a2, b2, 1e-7);
 } END_TEST
 
@@ -734,9 +769,26 @@ Suite* exp_suite(void) {
     return s;
 }
 
+Suite* log_suite(void) {
+    Suite* s;
+    TCase* tc_core;
+
+    s = suite_create("log");
+    tc_core = tcase_create("Core");
+    tcase_add_test(tc_core, s21_logClassic);
+    tcase_add_test(tc_core, s21_logBig);
+    tcase_add_test(tc_core, s21_logLow);
+    tcase_add_test(tc_core, s21_logZero);
+    tcase_add_test(tc_core, s21_logBelowzero);
+
+    suite_add_tcase(s, tc_core);
+
+    return s;
+}
+
 int main() {
     int no_failed = 0;
-    Suite *a, *b, *c, *d, *e, *f, *g, *h, *m, *l, /* *n, */ *j, *z, *t;
+    Suite *a, *b, *c, *d, *e, *f, *g, *h, *m, *l,  *n,  *j, *z, *t;
     SRunner* runner;
     a = floor_suite();
     b = tan_suite();
@@ -753,6 +805,8 @@ int main() {
     j = sin_suite();
     z = cos_suite();
     t = exp_suite();
+    n = log_suite();
+
     runner = srunner_create(a);
     srunner_add_suite(runner, b);
     srunner_add_suite(runner, c);
@@ -763,7 +817,7 @@ int main() {
     srunner_add_suite(runner, h);
     srunner_add_suite(runner, m);
     srunner_add_suite(runner, l);
-    // srunner_add_suite(runner, n);
+    srunner_add_suite(runner, n);
     srunner_add_suite(runner, j);
     srunner_add_suite(runner, z);
     srunner_add_suite(runner, t);
