cf38562ca3956f268b0fb8085cfc1dc0abf4fbc1
Nazeebo Delma <nazeebod@oa-i5.msk.21-school.ru>
2022-07-14 16:23:41 +0300

    :bug: update(s21_cos): fix big-low err

--- src/s21_cos.c
+++ src/s21_cos.c
@@ -2,7 +2,15 @@
 
 long double s21_cos(double x) {
     long double s21_cos = 0;
-    for (register int i = 0; i < 45; i++) {
+        while (x > 2 * S21_PI || x < -2 * S21_PI) {
+        if (x > 2 * S21_PI) {
+            x = x - 2 * S21_PI;
+        }
+        if (x < -2 * S21_PI) {
+            x = x + 2 * S21_PI;
+        }
+    }
+    for (register int i = 0; i < 50; i++) {
         s21_cos += s21_pow(-1, i) * ((s21_pow(x, 2 * i)) / (s21_fact(2 * i)));
     }
     return s21_cos;
