6a209b6c2923c0ab1974aade28c39dfdf3f1df5f
artemiikulikov <aapqwhat@gmail.com>
2022-07-16 23:41:49 +0300

    :bug: bug(log): fix test and err handling

--- src/s21_log.c
+++ src/s21_log.c
@@ -3,8 +3,13 @@
 long double s21_log(double x) {
     int ex_pow = 0;
     long double result = 0, compare = result;
-    if (x < 0) return S21_NAN;
-    if (x == 0) return -S21_INF;
+    if (x < 0) {
+        return S21_NAN;
+    } else if (x == 0) {
+        return -S21_INF;
+    } else if ((is_inf(x))) {
+        return S21_INF;
+    }
     for (; x >= S21_EXP; x /= S21_EXP, ex_pow++) continue;
 
     for (int i = 0; i < 100; i++) {
