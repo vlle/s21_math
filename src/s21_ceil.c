c24fb8c338085abaae5a4b920ba30b2696e1ae04
artemiikulikov <aapqwhat@gmail.com>
2022-07-16 23:25:28 +0300

    :bug: bug(tg func): updae func error handling

--- src/s21_ceil.c
+++ src/s21_ceil.c
@@ -2,7 +2,14 @@
 
 long double s21_ceil(double x) {
     long double y = (int) x;
-    if (x > y)
+    if ((is_nan(x))) { 
+        return (S21_NAN);
+    } else if ((is_inf(x))) {
+        return S21_INF;
+    }
+
+    if (x > y) {
         y += 1.0;
+    }
     return y;
 }
