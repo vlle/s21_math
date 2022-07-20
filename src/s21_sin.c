0979ea1315f5f4d1a4527454018207019686630d
Nazeebo Delma <nazeebod@oa-l3.msk.21-school.ru>
2022-07-20 20:52:35 +0300

    :bug: bug(s21_sin): fix timeout ERROR FROM SUPER_VERTER

--- src/s21_sin.c
+++ src/s21_sin.c
@@ -1,7 +1,6 @@
 #include "s21_math.h"
 
 long double s21_sin(double x) {
-    long double n = 0;
     long double sum = 0;
     if ((is_nan(x)) || (is_inf(x))) return S21_NAN;
     while (x > 2 * S21_PI || x < -2 * S21_PI) {
@@ -12,9 +11,8 @@ long double s21_sin(double x) {
             x = x + 2 * S21_PI;
         }
     }
-    while (n < 500) {
+    for (register int n = 0; n < 50; n++) {
         sum += s21_pow(-1, n) * s21_pow(x, 2*n + 1) / s21_fact(2*n + 1);
-        n++;
     }
     return sum;
 }
