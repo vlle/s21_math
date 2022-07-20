fcc0962774eba0b01d47863894f0bfda8a2fed9b
Nazeebo Delma <nazeebod@oa-l3.msk.21-school.ru>
2022-07-20 21:00:50 +0300

    :bug: bug(s21_fmod): add correct NAN for second meaning

--- src/s21_fmod.c
+++ src/s21_fmod.c
@@ -3,6 +3,7 @@
 long double s21_fmod(double x, double y) {
     long double ans = (long double) x;
     if ((is_nan(x)) || (is_inf(x))) return S21_NAN;
+    if ((is_nan(y)) || (is_inf(y))) return S21_NAN;
     if (y > 0) {
         if (ans >= 0) {
             while (ans >= y)
