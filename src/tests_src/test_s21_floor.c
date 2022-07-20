e164b1f279b1681d6242d1c94edf3ef27b6df97c
Nazeebo Delma <nazeebod@oa-l3.msk.21-school.ru>
2022-07-20 16:12:53 +0300

    :white_check_mark: test(floor): fix Coverage

--- src/tests_src/test_s21_floor.c
+++ src/tests_src/test_s21_floor.c
@@ -24,6 +24,13 @@ START_TEST(s21_floorClassic) {
     ck_assert_ldouble_eq_tol(a2, b2, 1e-7);
 } END_TEST
 
+START_TEST(s21_floorClassicNegative) {
+    double x = -1.788987;
+    long double a2 = floor(x);
+    long double b2 = s21_floor(x);
+    ck_assert_ldouble_eq_tol(a2, b2, 1e-7);
+} END_TEST
+
 
 START_TEST(s21_floorUnusual) {
     double x = 5.123231;
@@ -34,6 +41,13 @@ START_TEST(s21_floorUnusual) {
 
 START_TEST(s21_floorBig) {
     double x = 5.55555123123123213123123123;
+    long double a2 = floor(x);
+    long double b2 = s21_floor(x);
+    ck_assert_ldouble_eq_tol(a2, b2, 1e-7);
+} END_TEST
+
+START_TEST(s21_floorInt) {
+    double x = -6.000000;
     long double a2 = floor(x);
     long double b2 = s21_floor(x);
     ck_assert_ldouble_eq_tol(a2, b2, 1e-7);
@@ -50,6 +64,8 @@ Suite* floor_suite(void) {
     tcase_add_test(tc_core, s21_floorNan);
     tcase_add_test(tc_core, s21_floorClassic);
     tcase_add_test(tc_core, s21_floorUnusual);
+    tcase_add_test(tc_core, s21_floorClassicNegative);
+    tcase_add_test(tc_core, s21_floorInt);
     tcase_add_test(tc_core, s21_floorBig);
     suite_add_tcase(s, tc_core);
 
