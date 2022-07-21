ae7c2c3da2ec228e61294635bef58e782deb8629
Hotblack Helene <hotblack@oa-g6.msk.21-school.ru>
2022-07-21 10:37:58 +0300

    :bug: fixed fmod error

--- src/s21_fmod.c
+++ src/s21_fmod.c
@@ -2,20 +2,22 @@
 
 long double s21_fmod(double x, double y) {
     long double ans = (long double) x;
-    if ((is_nan(x)) || (is_inf(x))) return S21_NAN;
-    if (y > 0) {
-        if (ans >= 0) {
+    if ((is_nan(x)) || (is_inf(x) || is_nan(y)) || (is_inf(y) || y == 0)) {
+        ans = S21_NAN;
+    } else if (y > 0) {
+        if (ans >= 0)
             while (ans >= y)
                 ans -= y;
-        } else {
+        else
             while (ans <= -y)
                 ans += y;
-        }
-    } else if (y != 0) {
-        while (ans <= y)
-            ans -= y;
     } else {
-        ans = S21_NAN;
+        if (ans <= 0)
+            while (ans <= y)
+                ans -= y;
+        else
+            while (ans >= -y)
+                ans += y;
     }
     return ans;
 }
